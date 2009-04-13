078b7bc38d1fb8ca047f9c8ed63ac1e5b5b3bf12
Jason Harrelson <jason@lookforwardenterprises.com>
2009-04-13 10:56:59 -0500

    Release 1.0.4

--- lib/g_sectioned_shower.rb
+++ lib/g_sectioned_shower.rb
@@ -1,11 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'g_sectioned_shower/associative_resolver'
 require 'g_sectioned_shower/view_helpers'
 
 module GSectionedShower
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 end
 
 if defined?( ActionView::Base )
